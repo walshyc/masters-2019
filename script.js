0b7d751dad812cf5f29599f863c8e6ffb2d51614
Conor Walsh <conorwalsh0703@gmail.com>
2019-04-08 15:09:40 +0100

    added javascript to create table

--- script.js
+++ script.js
@@ -1,27 +1,92 @@
-var entries =[];
-$(document).ready(function () {
-    
-    $.getJSON('data.json', function (data) {
-        for (i = 0; i < data.length; i++) {
-            entries.push([data[i].name, data[i].picks[0], data[i].picks[1], data[i].picks[2]])
+var entries = [];
+var scores = []
 
-            $("#scoreboard-row").append(
-                `
-                <tr>
-                    <td>${entries[i][0]}</td>
-                    <td>${entries[i][1]}</td>
-                    <td>${entries[i][2]}</td>
-                    <td>${entries[i][3]}</td>
-                    <td>-12</td>
-                </tr>
-                `
-            );
-        }
 
-        console.log(entries)
-
-        
-
+var picks = [{
+        "name": "Conor Walsh",
+        "id": "1",
+        "selections": [
+            "Jonathan Byrd",
+            "Matt Kuchar",
+            "Jonathan Byrd"
+        ]
+    },
+    {
+        "name": "Martin Clarke",
+        "id": "2",
+        "selections": [
+            "Jim Furyk",
+            "Padraig Harrington",
+            "Aaron Baddeley"
+        ]
+    },
+    {
+        "name": "Brian Fahey",
+        "id": "3",
+        "selections": [
+            "Jhonattan Vegas",
+            "Abraham Ancer",
+            "Martin Kaymer"
+        ]
     }
-    );
+];
+
+
+$.getJSON('scores.json', function (data) {
+    var totalScore;
+
+    function getScores(name,pickOne, pickTwo, pickThree) {
+        for (i = 0; i < data.Leaderboards[0].Players.length; i++) {
+
+            if (data.Leaderboards[0].Players[i].Name === pickOne) {
+                var pickOneScore = data.Leaderboards[0].Players[i].Total;
+                console.log(pickOneScore);
+
+            }
+
+            if (data.Leaderboards[0].Players[i].Name === pickTwo) {
+                var pickTwoScore = data.Leaderboards[0].Players[i].Total;
+                console.log(pickTwoScore);
+
+
+            }
+
+            if (data.Leaderboards[0].Players[i].Name === pickThree) {
+                var pickThreeScore = data.Leaderboards[0].Players[i].Total;
+                console.log(pickThreeScore);
+            }
+        }
+    
+
+        totalScore = pickOneScore + pickTwoScore + pickThreeScore;
+        console.log(totalScore);
+        var obj = [];
+        obj["name"] = name;
+        obj["pickOne"] = pickOne;
+        obj["pickOneScore"] = pickOneScore;
+        obj["pickTwo"] = pickTwo;
+        obj["pickTwoScore"] = pickTwoScore;
+        obj["pickThree"] = pickThree;
+        obj["pickThreeScore"] = pickThreeScore;
+        obj["score"] = totalScore;
+        scores.push(obj);
+        
+    }
+
+    getScores(picks[0].name,picks[0].selections[0], picks[0].selections[1], picks[0].selections[2]);
+    getScores(picks[1].name,picks[1].selections[0], picks[1].selections[1], picks[1].selections[2]);
+    for (i = 0; i < scores.length; i++) {
+
+        $("#scoreboard-row").append(
+            `
+            <tr>  
+			<td >${scores[i].name}</td>
+			<td >${scores[i].score}</td>
+		    </tr>
+                `
+        );
+    }
+
+
 });
+
